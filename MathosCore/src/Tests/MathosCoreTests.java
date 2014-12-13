051038704db4558e615ca009615ad040944f9f92
artemlos <artem@artemlos.net>
2014-12-13 18:18:25 +0100

    Added Unit testing

---
+++ MathosCore/src/Tests/MathosCoreTests.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathosCoreTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
