63439ebbac8cfb7f18ec7bfaf6125249973779d6
artemlos <artem@artemlos.net>
2015-04-30 22:03:08 +0200

    Added Graph (HashGraph only)

---
+++ MathosCore/src/mathoscore/Data/Graph/VertexIterator.java
@@ -0,0 +1,28 @@
+package mathoscore.Data.Graph;
+
+import java.util.NoSuchElementException;
+
+/**
+ * An iterator over vertices in a Graph object.
+ * 
+ * @author Stefan Nilsson
+ * @version 2013-01-01
+ */
+
+public interface VertexIterator {
+	/**
+	 * Returns true if the iteration has more elements.
+	 * 
+	 * @return true if the iteration has more elements
+	 */
+	boolean hasNext();
+
+	/**
+	 * Returns the next element in the iteration.
+	 * 
+	 * @return the next element in the iteration
+	 * @throws NoSuchElementException
+	 *             if the iteration has no more elements
+	 */
+	int next() throws NoSuchElementException;
+}
